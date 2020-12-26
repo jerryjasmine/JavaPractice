838b781e3ee8bfd3a059b4b4dd56241271b90ea4
jerryjasmine <51484376+jerryjasmine@users.noreply.github.com>
2020-12-26 13:59:29 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PrimeNumber {
 	static Scanner s=new Scanner(System.in);
-
+//given no is prime no or not
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 int n;
