3e119f8f8edb569f5a7a8db443100069fb8b3296
jerryjasmine <51484376+jerryjasmine@users.noreply.github.com>
2020-12-26 12:21:28 +0530

    Add files via upload

---
+++ MinToYrsAndDays.java
@@ -0,0 +1,24 @@
+package java1;
+
+import java.util.*;
+
+public class MinToYrsAndDays {
+//converting minutes to days
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		long  t;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the minutes: ");
+		t=s.nextLong();
+		int yrs=(int)(t/525600);
+		int days=(int)(t/1440)%365;
+		System.out.println("years : "+yrs+"\nDays: "+days);
+		
+		
+		
+		
+
+	}
+
+}
