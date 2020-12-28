6c4761b2298b366ffdcc89c85cac5fc0af605cfd
jerryjasmine <51484376+jerryjasmine@users.noreply.github.com>
2020-12-28 14:26:59 +0530

    Update MinToYrsAndDays.java

--- MinToYrsAndDays.java
+++ MinToYrsAndDays.java
@@ -3,7 +3,7 @@ package java1;
 import java.util.*;
 
 public class MinToYrsAndDays {
-//converting minutes to years and days
+//converting minutes to days
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
@@ -13,8 +13,7 @@ public class MinToYrsAndDays {
 		t=s.nextLong();
 		int yrs=(int)(t/525600);
 		int days=(int)(t/1440)%365;
-		System.out.println("years : "+yrs+"\nDays: "+days);
-		
+		System.out.println(t+" Minutes is approximately "+ yrs +" years "+days+" days ");
 		
 		
 		
